165f3dbf03331e48e928e48130316fd17ba28b9a
Roman Kliuchko <roman@billie.io>
2020-04-18 19:31:49 +0200

    Udemy Node: Extend forecast data with humidity.

--- node/udemy_course/web-server/utils/forecast.js
+++ node/udemy_course/web-server/utils/forecast.js
@@ -9,7 +9,7 @@ module.exports = (latitiude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location.');
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + `. It is currently ${body.current.temperature} degrees out. Feels like ${body.current.feelslike} degrees out.`);
+            callback(undefined, body.current.weather_descriptions[0] + `. It is currently ${body.current.temperature} degrees out. Feels like ${body.current.feelslike} degrees out. The humidity is ${body.current.humidity}.`);
         }
     });
 }
