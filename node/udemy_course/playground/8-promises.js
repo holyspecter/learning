b5720ab8934a38b7dd8bf5d1e05254d3fab2f1eb
Roman Kliuchko <roman@billie.io>
2020-04-30 00:05:38 +0200

    Even more progress with Udemy Node.js course.

--- node/udemy_course/playground/8-promises.js
+++ node/udemy_course/playground/8-promises.js
@@ -1,12 +1,17 @@
-const doWorkPromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        // resolve([13, 4, 90]);
-        reject('This is my error!');
-    }, 2000);
-});
+const add = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            reject(new Error('test'));
+            resolve(a + b);
+        }, 2000);
+    })
+};
 
-doWorkPromise.then((result) => {
-    console.log('Success', result);
-}).catch((err) => {
-    console.log('Error', err);
+add(1, 2).then((sum) => {
+    console.log(sum);
+    return add(sum, 4);
+}).then((sum2) => {
+    console.log(sum2);
+}).catch((e) => {
+    console.log('meh');
 })
